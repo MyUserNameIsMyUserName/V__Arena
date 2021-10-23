86f284e7a7879e9f0b7b663c4f365f26133fd788
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-10-23 22:08:05 +0200

    big cleaning and going for fresh socket io chat app to modify

--- Var_v.0.1-57/helper_modules/is_dev/is_dev_mode.test.js
+++
@@ -1,5 +0,0 @@
-/*jshint esversion: 6 */
-const _DevMode = require("./is_dev_mode");
-
-console.log(`DEV_MODE >> ${_DevMode}`);
-console.log(`INVERT DEV_MODE >> ${!_DevMode}`);
--- Var_v.0.1-57/helper_modules/vfg_isDevMode/vfg_isDevMode.js
+++
@@ -1,7 +0,0 @@
-const V_App_Config = require("../../.Vfg.js");
-
-const isDevMode = () => { 
-    return V_App_Config.V_DEV;
-}
-
-module.exports = isDevMode;
